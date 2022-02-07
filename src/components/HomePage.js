889edc759258ea6cbd8f887e4ef4614093902165
michaelbruns1 <mb@michaelbruns.one>
2022-02-06 18:30:43 -0800

    3 files modified, 1 added

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,7 +5,7 @@ function HomePage() {
     <div className="jumbotron bg-light rounded-3">
       <h1>Pluralsight Administration</h1>
       <p>React, Flux, and React Router for ultra-responsive web apps.</p>
-      <a href="/about">About</a>
+      <a href="/about">About</a> | <a href="/courses">Courses</a>
     </div>
   );
 }
